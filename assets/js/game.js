269f9db599f3e36445538a03b14bc67bc6cdab99
sshahram <shirin.shahram23@gmail.com>
2021-02-13 18:28:37 -0800

    Use JavaScript functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+// this creates a function named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+//fight();
